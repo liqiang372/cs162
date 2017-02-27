3c88b72135f9a61fce743bd9aa7a68ebce207e21
drbelfast <liqiang372@gmail.com>
2017-02-27 01:05:04 +0000

    HW0 done

--- hw0/main.c
+++ hw0/main.c
@@ -3,8 +3,13 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    struct rlimit rlp;
+    struct rlimit fsize;
+    getrlimit(RLIMIT_STACK, &lim);
+    getrlimit(RLIMIT_NPROC, &rlp);
+    getrlimit(RLIMIT_FSIZE, &fsize);
+    printf("stack size: %ld\n", lim.rlim_cur);
+    printf("process limit: %ld\n", rlp.rlim_cur);
+    printf("max file descriptors: %ld\n", fsize.rlim_cur);
     return 0;
 }
