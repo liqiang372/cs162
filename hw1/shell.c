5891a5caa35747c3687d2b4fd1e4d347ffcd8f90
drbelfast <liqiang372@gmail.com>
2017-03-20 05:30:44 +0000

    Finished adding basic functionality into the shell

--- hw1/shell.c
+++ hw1/shell.c
@@ -30,6 +30,7 @@ pid_t shell_pgid;
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
 int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -44,7 +45,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
-  {cmd_pwd, "pwd", "prints the current working directory"}
+  {cmd_pwd, "pwd", "prints the current working directory"},
+  {cmd_cd, "cd", "directing to folders"}
 };
 
 /* Prints a helpful description for the given command */
@@ -66,7 +68,14 @@ int cmd_pwd(unused struct tokens *tokens) {
     printf("%s\n", cwd);
   else
     perror("error occured");
-  return 0;
+  return 1;
+}
+
+/* get into provided directory*/
+
+int cmd_cd(unused struct tokens *tokens) {
+  chdir(tokens_get_token(tokens, 1));
+  return 1;
 }
 
 /* Looks up the built-in command, if it exists. */
